d7b26dfb40555fd82f768bfa0774af41eff2530d
Zulfikar_Pra <fikarpra@mail.ugm.ac.id>
2023-07-08 17:59:00 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
