067b7878c03715f0cf1e1ad444e7711a435ad83b
nashiraoksani <nashiraaoksani@mail.ugm.ac.id>
2023-07-29 11:52:05 +0700

    add logres texture

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
-import { Header } from './components/Header';
-import './style.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Header></Header> */}
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
           <Route path="/login" element={<Login></Login>}></Route>
