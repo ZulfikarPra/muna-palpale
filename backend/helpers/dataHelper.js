d6a61859b62540b167f4a19b62ea4f80f91598f1
Zulfikar_Pra <fikarpra@mail.ugm.ac.id>
2023-08-04 23:34:37 +0700

    feat: image upload (not finished)

---
+++ backend/helpers/dataHelper.js
@@ -0,0 +1,6 @@
+const Multer = require('multer');
+
+const storage = new Multer.memoryStorage();
+const upload = Multer({storage});
+
+module.exports = upload;
--- backend/routes/articles-route.js
+++ backend/routes/articles-route.js
@@ -6,10 +6,13 @@ const {
   createArticle,
   updateArticle,
   deleteArticle,
+  handleUpload,
 } = require('../controllers/articles-controller');
+
 const {
   adminProtect,
 } = require('../middleware/articles-middleware');
+const upload = require('../helpers/dataHelper');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
@@ -17,8 +20,18 @@ const router = express.Router();
 router.route('/').get(getArticles).post(createArticle);
 router.route('/:id').get(getArticleById).put(updateArticle).delete(deleteArticle);
 router.route('/tags/:tag').get(getFilteredArticles);
-router.route('/create').post(adminProtect, createArticle);
-router.route('/delete/:id').delete(adminProtect, deleteArticle);
-router.route('/update/:id').put(adminProtect, updateArticle);
+
+router.route('/create').post(upload.single('photos'), async (req, res) => {
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString('base64');
+    const dataURI = 'data:' + req.file.mimetype + ';base64,' + b64;
+    await handleUpload(dataURI);
+  } catch (err) {
+    console.log(err);
+  }
+}, createArticle);
+
+router.route('/delete/:id').delete(deleteArticle);
+router.route('/update/:id').put(updateArticle);
 
 module.exports = router;
