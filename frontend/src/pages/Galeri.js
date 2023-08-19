a0c78123aedf23cba0a249f43de4a89dc90bd6a9
nashiraoksani <nashiraaoksani@mail.ugm.ac.id>
2023-08-19 16:52:41 +0700

    add galeri images

--- frontend/src/pages/Galeri.js
+++ frontend/src/pages/Galeri.js
@@ -1,4 +1,5 @@
 import blank from '../asset/blank.png';
+import GaleriImg from '../asset/galeri/GaleriImg';
 
 export const Galeri = () => {
   return (
@@ -7,6 +8,34 @@ export const Galeri = () => {
         <h1>Galeri</h1>
       </div>
       <div className="galeri-content">
+        <img src={GaleriImg[1]} alt="" className="child" />
+        <img src={GaleriImg[2]} alt="" className="child" />
+        <img src={GaleriImg[3]} alt="" className="child" />
+        <img src={GaleriImg[4]} alt="" className="child" />
+        <img src={GaleriImg[5]} alt="" className="child" />
+        <img src={GaleriImg[6]} alt="" className="child" />
+        <img src={GaleriImg[7]} alt="" className="child" />
+        <img src={GaleriImg[8]} alt="" className="child" />
+        <img src={GaleriImg[9]} alt="" className="child" />
+        <img src={GaleriImg[10]} alt="" className="child" />
+        <img src={GaleriImg[11]} alt="" className="child" />
+        <img src={GaleriImg[12]} alt="" className="child" />
+        <img src={GaleriImg[13]} alt="" className="child" />
+        <img src={GaleriImg[14]} alt="" className="child" />
+        <img src={GaleriImg[15]} alt="" className="child" />
+        <img src={GaleriImg[16]} alt="" className="child" />
+        <img src={GaleriImg[17]} alt="" className="child" />
+        <img src={GaleriImg[18]} alt="" className="child" />
+        <img src={GaleriImg[19]} alt="" className="child" />
+        <img src={GaleriImg[20]} alt="" className="child" />
+        <img src={GaleriImg[21]} alt="" className="child" />
+        <img src={GaleriImg[22]} alt="" className="child" />
+        <img src={GaleriImg[23]} alt="" className="child" />
+        <img src={GaleriImg[24]} alt="" className="child" />
+        <img src={GaleriImg[25]} alt="" className="child" />
+        <img src={GaleriImg[26]} alt="" className="child" />
+        <img src={GaleriImg[27]} alt="" className="child" />
+        {/* <img src={blank} alt="" className="child" />
         <img src={blank} alt="" className="child" />
         <img src={blank} alt="" className="child" />
         <img src={blank} alt="" className="child" />
@@ -21,10 +50,7 @@ export const Galeri = () => {
         <img src={blank} alt="" className="child" />
         <img src={blank} alt="" className="child" />
         <img src={blank} alt="" className="child" />
-        <img src={blank} alt="" className="child" />
-        <img src={blank} alt="" className="child" />
-        <img src={blank} alt="" className="child" />
-        <img src={blank} alt="" className="child" />
+        <img src={blank} alt="" className="child" /> */}
       </div>
     </div>
   );
