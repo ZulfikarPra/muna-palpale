68e9bd6775409e48b3f077be3272ae90ff2da992
nashiraoksani <nashiraaoksani@mail.ugm.ac.id>
2023-08-20 10:46:23 +0700

    daya tarik dropdown mobile

--- frontend/src/pages/DayaTarik.js
+++ frontend/src/pages/DayaTarik.js
@@ -6,19 +6,83 @@ import { useState } from 'react';
 
 export const DayaTarik = () => {
   const [show, setShow] = useState(true);
+  const [menuText, setMenuText] = useState('Alam Desa');
+  const [isActive, setIsActive] = useState(false);
 
   return (
     <div className="daya-tarik">
       <div className="daya-tarik-jumbotron">
         <h1>Daya Tarik</h1>
       </div>
-      <div className="daya-tarik-button">
-        <div className="show-btn-container">
-          <button className="show-btn" onClick={() => setShow(!show)}>
-            Alam Desa
-          </button>
+      <div className="daya-tarik-menu">
+        <div className="dropdown-mobile">
+          <div className="show-btn-container">
+            <button
+              className="show-btn"
+              style={{
+                backgroundColor: isActive ? 'orange' : 'white',
+                color: isActive ? 'white' : 'orange',
+              }}
+              onClick={() => {
+                setShow(!show);
+                setIsActive(!isActive);
+              }}
+            >
+              {menuText}
+            </button>
+          </div>
+          {show === true ? (
+            <ul>
+              <li>
+                <button
+                  onClick={(e) => {
+                    setMenuText(e.target.innerText);
+                    setShow(!show);
+                    setIsActive(!isActive);
+                  }}
+                >
+                  Alam Desa
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={(e) => {
+                    setMenuText(e.target.innerText);
+                    setShow(!show);
+                    setIsActive(!isActive);
+                  }}
+                >
+                  Sosial Budaya
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={(e) => {
+                    setMenuText(e.target.innerText);
+                    setShow(!show);
+                    setIsActive(!isActive);
+                  }}
+                >
+                  Makanan Khas
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={(e) => {
+                    setMenuText(e.target.innerText);
+                    setShow(!show);
+                    setIsActive(!isActive);
+                  }}
+                >
+                  Bahasa Muna
+                </button>
+              </li>
+            </ul>
+          ) : (
+            ''
+          )}
         </div>
-        {show === true ? (
+        <div className="menu-desktop">
           <ul>
             <li>
               <button>Alam Desa</button>
@@ -33,9 +97,7 @@ export const DayaTarik = () => {
               <button>Bahasa Muna</button>
             </li>
           </ul>
-        ) : (
-          ''
-        )}
+        </div>
       </div>
       <div className="daya-tarik-content">
         <CardLeft></CardLeft>
