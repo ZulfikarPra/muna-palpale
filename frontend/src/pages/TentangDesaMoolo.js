df43351f5debf2c30d25cedb2bec7fa8083832a8
nashiraoksani <nashiraaoksani@mail.ugm.ac.id>
2023-10-07 20:25:44 +0700

    visi misi desa moolo

--- frontend/src/pages/TentangDesaMoolo.js
+++ frontend/src/pages/TentangDesaMoolo.js
@@ -1,3 +1,6 @@
+import imgProfilMoolo from '../asset/galeri/12 kalimalima.webp';
+import imgKepalaDesaMoolo from '../asset/kepala-desa-moolo.jpg';
+
 export const TentangDesaMoolo = () => {
   return (
     <div className="tentang-desa">
@@ -10,23 +13,14 @@ export const TentangDesaMoolo = () => {
       <section id="judulDesa">
         <h2>Desa Moolo</h2>
         <div className="img">
-          <img src="" alt="" />
+          <img src={imgProfilMoolo} alt="" />
         </div>
       </section>
       <section id="visiMisi">
         <h2>Visi</h2>
-        <h4>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci impedit praesentium ex facilis, illo, laudantium dignissimos, tempora similique ab nulla accusamus voluptates quo nihil ipsam. Ab eligendi aut libero sint sequi
-          esse voluptatibus quia sunt! Cum inventore, praesentium exercitationem ullam distinctio perspiciatis itaque. Quidem fuga soluta sit, asperiores necessitatibus illo.
-        </h4>
+        <h4>“Mewujudkan Desa Moolo lebih maju, mandiri, dan religious melalui Pembangunan partisipatif dan tata kelola pemerintahan yang baik.”</h4>
         <h2>Misi</h2>
-        <ol>
-          <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi cupiditate, rem maiores error blanditiis culpa, voluptates magnam quibusdam provident alias eum ut magni sequi officia iste accusantium laborum veritatis molestias saepe, sint similique fugiat odit recusandae at. Impedit eligendi ab aliquam fugiat illo similique, cum expedita aspernatur consequatur nam. Dolore reprehenderit aspernatur culpa similique necessitatibus illo a soluta! Animi nihil laborum voluptas, fuga cum incidunt harum aliquam quam! Laboriosam eveniet aspernatur temporibus magnam. Numquam, recusandae? Iusto ab porro repellat laborum tenetur quaerat ad architecto illum dolor illo officiis libero, enim est perspiciatis dicta maxime asperiores, esse vel itaque unde excepturi quidem, quo distinctio. Odit, dolorum vel similique maiores dolorem minus explicabo vitae amet cum illum enim eligendi alias sed eius ut est voluptas praesentium in illo atque molestiae possimus non. Minima, non sequi. Aliquid distinctio, eum reiciendis asperiores iste dolores! Tempore nemo eligendi velit quisquam, laudantium harum necessitatibus corrupti reprehenderit.</li>
-          <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi cupiditate, rem maiores error blanditiis culpa, voluptates magnam quibusdam provident alias eum ut magni sequi officia iste accusantium laborum veritatis molestias saepe, sint similique fugiat odit recusandae at. Impedit eligendi ab aliquam fugiat illo similique, cum expedita aspernatur consequatur nam. Dolore reprehenderit aspernatur culpa similique necessitatibus illo a soluta! Animi nihil laborum voluptas, fuga cum incidunt harum aliquam quam! Laboriosam eveniet aspernatur temporibus magnam. Numquam, recusandae? Iusto ab porro repellat laborum tenetur quaerat ad architecto illum dolor illo officiis libero, enim est perspiciatis dicta maxime asperiores, esse vel itaque unde excepturi quidem, quo distinctio. Odit, dolorum vel similique maiores dolorem minus explicabo vitae amet cum illum enim eligendi alias sed eius ut est voluptas praesentium in illo atque molestiae possimus non. Minima, non sequi. Aliquid distinctio, eum reiciendis asperiores iste dolores! Tempore nemo eligendi velit quisquam, laudantium harum necessitatibus corrupti reprehenderit.</li>
-          <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi cupiditate, rem maiores error blanditiis culpa, voluptates magnam quibusdam provident alias eum ut magni sequi officia iste accusantium laborum veritatis molestias saepe, sint similique fugiat odit recusandae at. Impedit eligendi ab aliquam fugiat illo similique, cum expedita aspernatur consequatur nam. Dolore reprehenderit aspernatur culpa similique necessitatibus illo a soluta! Animi nihil laborum voluptas, fuga cum incidunt harum aliquam quam! Laboriosam eveniet aspernatur temporibus magnam. Numquam, recusandae? Iusto ab porro repellat laborum tenetur quaerat ad architecto illum dolor illo officiis libero, enim est perspiciatis dicta maxime asperiores, esse vel itaque unde excepturi quidem, quo distinctio. Odit, dolorum vel similique maiores dolorem minus explicabo vitae amet cum illum enim eligendi alias sed eius ut est voluptas praesentium in illo atque molestiae possimus non. Minima, non sequi. Aliquid distinctio, eum reiciendis asperiores iste dolores! Tempore nemo eligendi velit quisquam, laudantium harum necessitatibus corrupti reprehenderit.</li>
-          <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi cupiditate, rem maiores error blanditiis culpa, voluptates magnam quibusdam provident alias eum ut magni sequi officia iste accusantium laborum veritatis molestias saepe, sint similique fugiat odit recusandae at. Impedit eligendi ab aliquam fugiat illo similique, cum expedita aspernatur consequatur nam. Dolore reprehenderit aspernatur culpa similique necessitatibus illo a soluta! Animi nihil laborum voluptas, fuga cum incidunt harum aliquam quam! Laboriosam eveniet aspernatur temporibus magnam. Numquam, recusandae? Iusto ab porro repellat laborum tenetur quaerat ad architecto illum dolor illo officiis libero, enim est perspiciatis dicta maxime asperiores, esse vel itaque unde excepturi quidem, quo distinctio. Odit, dolorum vel similique maiores dolorem minus explicabo vitae amet cum illum enim eligendi alias sed eius ut est voluptas praesentium in illo atque molestiae possimus non. Minima, non sequi. Aliquid distinctio, eum reiciendis asperiores iste dolores! Tempore nemo eligendi velit quisquam, laudantium harum necessitatibus corrupti reprehenderit.</li>
-          <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi cupiditate, rem maiores error blanditiis culpa, voluptates magnam quibusdam provident alias eum ut magni sequi officia iste accusantium laborum veritatis molestias saepe, sint similique fugiat odit recusandae at. Impedit eligendi ab aliquam fugiat illo similique, cum expedita aspernatur consequatur nam. Dolore reprehenderit aspernatur culpa similique necessitatibus illo a soluta! Animi nihil laborum voluptas, fuga cum incidunt harum aliquam quam! Laboriosam eveniet aspernatur temporibus magnam. Numquam, recusandae? Iusto ab porro repellat laborum tenetur quaerat ad architecto illum dolor illo officiis libero, enim est perspiciatis dicta maxime asperiores, esse vel itaque unde excepturi quidem, quo distinctio. Odit, dolorum vel similique maiores dolorem minus explicabo vitae amet cum illum enim eligendi alias sed eius ut est voluptas praesentium in illo atque molestiae possimus non. Minima, non sequi. Aliquid distinctio, eum reiciendis asperiores iste dolores! Tempore nemo eligendi velit quisquam, laudantium harum necessitatibus corrupti reprehenderit.</li>
-        </ol>
+        <h4>“Menjadikan Desa Moolo maju di bidang pertanian, pariwisata, transparasi, empati, berprestasi, aspiratif, dan bernafaskan islami.” </h4>
       </section>
       <section id="videoProfile">
         <h2>Video Profile</h2>
@@ -37,7 +31,7 @@ export const TentangDesaMoolo = () => {
       <section id="kepalaDesa">
         <h2>Kepala Desa</h2>
         <div className="img">
-          <img src="" alt="" />
+          <img src={imgKepalaDesaMoolo} alt="" />
         </div>
         <div className="text">
           <h3>Natsir S.Sos</h3>
@@ -46,23 +40,21 @@ export const TentangDesaMoolo = () => {
       </section>
       <section id="kondisiGeografi">
         <h2>Kondisi Geografi</h2>
-      <div className="img">
-        <img src="" alt="" />
-      </div>
-      <h4>( Desa Moolo, Kec.Batukara, Kab.Muna, Sulawesi Tenggara )</h4>
+        <div className="img">
+          <img src="" alt="" />
+        </div>
+        <h4>( Desa Moolo, Kec.Batukara, Kab.Muna, Sulawesi Tenggara )</h4>
       </section>
       <section id="kondisiDemografi">
-      <h2>Kondisi Demografi</h2>
-      <h4>Jumlah Penduduk : </h4>
-      <h4>Luas Desa : </h4>
-      <h4>Kepadatan Penduduk : </h4>
-      <h4>Banyaknya Rumah Tangga : </h4>
+        <h2>Kondisi Demografi</h2>
+        <h4>Jumlah Penduduk : </h4>
+        <h4>Luas Desa : </h4>
+        <h4>Kepadatan Penduduk : </h4>
+        <h4>Banyaknya Rumah Tangga : </h4>
       </section>
       <section id="peraturanDesa">
         <h2>Peraturan Desa</h2>
-        <div className="pdfViewer">
-
-        </div>
+        <div className="pdfViewer"></div>
       </section>
     </div>
   );
